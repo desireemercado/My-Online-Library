e7065e3447324d4d2bb467745f0e8bdfc77e3eb2
Desiree <desiree.mercado@live.lagcc.cuny.edu>
2017-12-05 18:26:07 -0500

    Add files via upload

---
+++ my online library/inc/data.php
@@ -0,0 +1,135 @@
+<?php
+$catalog = [];
+//Books
+$catalog[101] = [
+	"title" => "Confessions of a Murder Suspect",
+	"img" => "img/media/book1.jpg",
+    "genre" => "Crime",
+    "format" => "Paperback",
+    "year" => 2013,
+    "category" => "Books",
+    "authors" => [
+        "James Patterson"
+        "Maxine Paetro"
+    ],
+    "publisher" => "Little, Brown and Company",
+    "isbn" => '0316206989'
+];
+
+$catalog[102] = [
+	"title" => "American Gods",
+	"img" => "img/media/book2.jpg",
+    "genre" => "Fantasy",
+    "format" => "Paperback",
+    "year" => 2001,
+    "category" => "Books",
+    "authors" => [
+        "Neil Gaiman"
+    ],
+    "publisher" => "HarperCollins, Headline Publishing Group",
+    "isbn" => '9780061122910'
+];
+
+$catalog[103] = [
+	"title" => "Pretty Girl-13",
+	"img" => "img/media/book3.jpg",
+    "genre" => "Suspense",
+    "format" => "Paperback",
+    "year" => 2013,
+    "category" => "Books",
+    "authors" => [
+        "Liz Coley"
+    ],
+    "publisher" => "HarperCollins Publishers",
+    "isbn" => '9780062127396'
+];
+
+//Movies
+$catalog[201] = [
+    "title" => "The Sound of Music",
+    "img" => "img/media/movie3.jpg",
+    "genre" => "Drama/Romance",
+    "format" => "DVD",
+    "year" => 1965(USA),
+    "category" => "Movies",
+    "director" => "Robert Wise",
+    "writers" => [
+        "George Hurdalek",
+        "Howard Lindsay",
+
+    ],
+    "stars" => [
+        "Julie Andrews",
+        "Christoper Plummer",
+        "Elenor Parker",
+        "Richard Haydn"
+    ]
+];
+
+$catalog[202] = [
+    "title" => "Let the Right one In",
+    "img" => "img/media/movie5.jpg",
+    "genre" => "Drama/Horror/Romance",
+    "format" => "DVD",
+    "year" => 2008,
+    "category" => "Movies",
+    "director" => "Tomas Alfredson",
+    "writers" => [
+        "John Ajvide Lindqvist"
+
+    ],
+    "stars" => [
+        " Kåre Hedebrant",
+        "Lina Leandersson",
+        "Per Ragnar"
+    ]
+];
+
+$catalog[203] = [
+    "title" => "The Girl with all the Gifts",
+    "img" => "img/media/movie4.jpg",
+    "genre" => "Drama/Horror/Thriller",
+    "format" => "DVD",
+    "year" => 2016,
+    "category" => "Movies",
+    "director" => "Colm McCarthy",
+    "writers" => [
+        "Mike Carey"
+
+    ],
+    "stars" => [
+        "Sennia Nanua",
+        "Fisayo Akinade",
+        "Dominique",
+        "Paddy Considine"
+    ]
+];
+
+//Music
+$catalog[301] = [
+    "title" => "Girls like Girls",
+    "img" => "img/media/album1.jpg",
+    "genre" => "Pop",
+    "format" => "CD",
+    "year" => 2015,
+    "category" => "Music",
+    "artist" => "Haley Kiyoko"
+];
+$catalog[302] = [
+    "title" => "Sleepover",
+    "img" => "img/media/album3.jpg",
+    "genre" => "Pop",
+    "format" => "CD",
+    "year" => 2017,
+    "category" => "Music",
+    "artist" => "Haley Kiyoko"
+];
+$catalog[303] = [
+    "title" => "Cliff's Edge",
+    "img" => "img/media/album5.jpg",
+    "genre" => "Pop",
+    "format" => "CD",
+    "year" => 2015,
+    "category" => "Music",
+    "artist" => "Haley Kiyoko"
+];
